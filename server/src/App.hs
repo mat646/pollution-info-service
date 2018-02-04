8455fdaf84653a483323a47084f18261c4d3d5c5
mateusz <mat646@gmail.com>
2018-02-04 12:31:04 +0100

    Implemented caching.

--- server/src/App.hs
+++ server/src/App.hs
@@ -25,6 +25,8 @@ import           Station
 import           Sensor
 import           Survey
 import           Table
+import           Data.Time
+import           Data.Map
 
 type WithAssets = Api :<|> Raw
 
@@ -38,16 +40,48 @@ app =
 server :: IO (Server WithAssets)
 server = do
   assets <- serveAssets
-  return $ apiServer :<|> assets
+  tabMap <- DB <$> newMVar Data.Map.empty
 
-apiServer :: Server Api
-apiServer =
-  (liftIO $ (getTable 1)) :<|>
-  (liftIO $ (getTable 2)) :<|>
-  (liftIO $ (getTable 3))
+  return $ apiServer tabMap :<|> assets
 
+apiServer :: DB -> Server Api
+apiServer tabMap =
+  (liftIO $ (process 1 tabMap)) :<|>
+  (liftIO $ (process 2 tabMap)) :<|>
+  (liftIO $ (process 3 tabMap))
+
+
+
+data DB = DB (MVar (Map Int Table))
 
 -- handling sensors
+
+process :: Int -> DB -> IO Table
+process n (DB mvar) = modifyMVar mvar $ \ mapTab -> do
+  case (Data.Map.lookup n mapTab) of
+    Nothing -> do
+      genTable <- getTable n
+      return (insert n genTable mapTab, genTable)
+    Just tab -> do
+      timeDiff <- getTimeDiff (Table.time tab)
+      print $ timeDiff
+      if timeDiff < 4000.0
+        then return (insert n tab mapTab, tab)
+        else do
+          genTable <- getTable n
+          return (insert n genTable mapTab, genTable)
+
+
+getTimeDiff :: String -> IO Double
+getTimeDiff str = do
+  case str of
+    "null" -> return 5000.0
+    _ -> do
+      time <- getCurrentTime
+      let local = read (str) :: LocalTime
+      let abs = localTimeToUTC (minutesToTimeZone 60) local
+      let diff = diffUTCTime time abs
+      return (realToFrac diff)
 
 getTable :: Int -> IO Table
 getTable n = do
@@ -60,14 +94,13 @@ retStation :: Int -> IO Station
 retStation n = do
   manager <- newManager defaultManagerSettings
   let url n = case n of
-        1 -> "http://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/9153"
+        1 -> "http://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/402" --9153
         2 -> "http://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/400"
         _ -> "http://api.gios.gov.pl/pjp-api/rest/aqindex/getIndex/10120"
   request <- parseRequest (url n)
   response <- httpLbs request manager
   let result = responseBody response
   let parsed = decode result :: Maybe Station
-  --print $ parsed
   return (fromJust parsed)
 
 
@@ -75,7 +108,7 @@ getSensorIds :: Int -> IO [String]
 getSensorIds n =
   case n of
     1 -> return ["14707", "14730", "14734", "14727", "14731", "14729", "14733"]
-    2 -> return ["2745","2750","16500","2747","2752","400", "400"]
+    2 -> return ["400","2750","16500","2747","2752","400", "400"]
     _ -> return ["16465","16457","400","16460","16494","400", "400"]
 
 
@@ -124,6 +157,12 @@ unwrap dob =
     Nothing -> 0
     Just x -> x
 
+unwrapTime :: Maybe String -> String
+unwrapTime time =
+  case time of
+    Nothing -> "null"
+    Just x -> x
+
 unwrapIO :: IO Station -> ( Station -> Maybe Index ) -> IO String
 unwrapIO station fun = do
   wrappedStation <- station
@@ -134,7 +173,7 @@ unwrapIO station fun = do
 createTable :: IO Station -> [Double] -> IO Table
 createTable station doubl = do
   ids <- return . Station.id =<< station
-  times <- return . fromJust . Station.stCalcDate =<< station
+  times <- return . unwrapTime . Station.stCalcDate =<< station
   so2 <- unwrapIO station Station.so2IndexLevel
   so2Double <- return (doubl !! 6)
   no2 <- unwrapIO station Station.no2IndexLevel
